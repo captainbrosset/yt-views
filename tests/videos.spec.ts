125309a7709e48e6c480374847c2abdeeb4918b0
Patrick Brosset <patrickbrosset@gmail.com>
2023-02-27 09:09:05 +0100

    Stop tracking 109

--- tests/videos.spec.ts
+++ tests/videos.spec.ts
@@ -17,7 +17,7 @@ const VIDEO_URLS = [
   // { id: 'QcAnrbfJN0s', name: 'whats new 107' },
   // { id: '2fuTHag4EOw', name: 'OLD whats new 108' },
   // { id: 'KmGXnsSCtI4', name: 'whats new 108' },
-  { id: 'b-q-PLmO-ns', name: 'whats new 109' },
+  // { id: 'b-q-PLmO-ns', name: 'whats new 109' },
   { id: 'qrzq3bN_1BY', name: 'whats new 110' },
 ];
 
